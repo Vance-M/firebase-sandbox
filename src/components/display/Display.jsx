bca98d47403338acb6e0cfdbd1fecac25b6f16c6
Vance-M <myveryvills@yahoo.com>
2021-06-28 15:18:39 -0700

    displays all database entrees

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,17 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-
-export default function Display({ user }) {
+export default function Display(
+    {                        
+        username,
+        name,
+        link,
+        repos,
+        gists,
+        followers,
+        following,
+        creation 
+    }) {
     return (
-        <div>
-            user
-        </div>
+        <figure>
+        <a href={link}>{username}</a>
+        <p>Name: {name}</p>
+        <p>Repos: {repos}</p>
+        <p>Gists: {gists}</p>
+        <p>Followers: {followers}</p>
+        <p>Following: {following}</p>
+        <p>Created: {creation}</p>
+
+      </figure>
     )
 }
 
 
 Display.propTypes = {
-    user: PropTypes.object.isRequired,
+    username: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    repos: PropTypes.number.isRequired,
+    gists: PropTypes.number.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    creation: PropTypes.string.isRequired
   };
   

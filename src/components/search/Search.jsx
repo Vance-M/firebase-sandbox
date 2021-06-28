7325a931acd081ef8f8b13cc4f1b58f4cd9d7bcb
Vance-M <myveryvills@yahoo.com>
2021-06-28 12:20:54 -0700

    Search form with enter button

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 
-export default function Search() {
+export default function Search({onSubmit, query, onQueryChange}) {
     return (
-        <div>
-            Hello.
-        </div>
+        <form onSubmit={onSubmit}>
+        <label htmlFor="gitUser-search">
+          <input
+            id="gitUser-search"
+            placeholder="Github User Search"
+            type="text"
+            value={query}
+            onChange={onQueryChange}
+            data-testid="gitUser-search"
+          />
+        </label>
+        {/* <input type="submit" value="Submit"/> */}
+      </form>
     )
 }
 
-
+Search.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    query: PropTypes.string.isRequired,
+    onQueryChange: PropTypes.func.isRequired,
+  };
+  
 
--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,11 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Search from '../../components/search/Search.jsx'
 import Display from '../../components/display/Display.jsx'
+
+
+
+
 function App() {
+
+  const [query, setQuery] = useState('');
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(query);
+    // getUsers(query)
+    //   .then(setArtists)
+  };
+
+  const onQueryChangeHandler = ({ target }) => {
+    setQuery(target.value);
+  };
+
 
   return (
     <>
-      <Search />
+      <Search
+        onSubmit={onSubmitHandler}
+        query={query}
+        onQueryChange={onQueryChangeHandler}
+      />
       <Display />
     </>
   );
